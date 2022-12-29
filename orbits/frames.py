94cebffa109b3931f6ad93b64daa060c26da779f
Nick Thurmes <nthurmes@gmail.com>
2022-12-28 18:54:24 -0700

    Copy everything in-line

---
+++ orbits/frames.py
@@ -0,0 +1,77 @@
+from math import sin, cos, atan2, sqrt
+
+import networkx as nx
+import numpy as np
+
+
+def rotations(pairs):
+    dcms = {
+        1: lambda a: np.array([[1, 0, 0], [0, cos(a), sin(a)], [0, -sin(a), cos(a)]]),
+        2: lambda a: np.array([[cos(a), 0, -sin(a)], [0, 1, 0], [sin(a), 0, cos(a)]]),
+        3: lambda a: np.array([[cos(a), sin(a), 0], [-sin(a), cos(a), 0], [0, 0, 1]]),
+    }
+    mat = np.identity(3)
+    for ax, theta in pairs:
+        dcm = dcms[ax](theta)
+        mat = dcm @ mat
+    return mat
+
+
+def _get_dcm(transform, **params) -> np.ndarray:
+    if callable(transform):
+        # a function that returns either a matrix or axis, angle pairs
+        return _get_dcm(transform(**params), **params)
+    if isinstance(transform, np.ndarray) and transform.shape == (3, 3):
+        # full matrix
+        return transform
+    # iterable of (axis, angle) pairs
+    return rotations(transform)
+
+
+def _get_vector(transform, **params) -> np.ndarray:
+    if callable(transform):
+        return _get_vector(transform(**params), **params)
+    if isinstance(transform, np.ndarray) and transform.shape == (3, 1):
+        return transform
+    vec = np.array(transform)
+    return vec.reshape((3, 1))
+
+
+class FrameSystem(nx.DiGraph):
+    def add_frame(self, name, parent=None, rotation=None, translation=None, inv_translation=None):
+        # translation is expressed in the parent frame and is a vector from parent to child
+        # inv_translation is also vector from parent to child, but in the child frame
+        self.add_node(name)
+        if not parent or not rotation:
+            return
+        if translation is None and inv_translation is None:
+            translation = _get_vector([0, 0, 0])
+        if translation is not None:
+            # translation = lambda **p: _get_vector(translation, **p)
+            inverse_translation = lambda **p: _get_dcm(rotation, **p) @ -_get_vector(translation, **p)
+        elif inv_translation is not None:
+            inverse_translation = lambda **p: -_get_vector(inv_translation, **p)
+            translation = lambda **p: _get_dcm(rotation, **p).transpose() @ _get_vector(inv_translation, **p)
+        else:
+            raise ValueError("Must have none or exactly one of translation and inv_translation")
+        self.add_edge(parent, name, tr=translation, rot=rotation)
+        inverse_rotation = lambda **p: _get_dcm(rotation, **p).transpose()
+        self.add_edge(name, parent, tr=inverse_translation, rot=inverse_rotation)
+
+    def transform(self, a, b, **params):
+        path = nx.shortest_path(self, a, b)
+        rotation = np.identity(3)
+        translation = np.zeros((3, 1))
+        for edge in zip(path[:-1], path[1:]):
+            attrs = self.get_edge_data(*edge)
+            matrix = _get_dcm(attrs["rot"], **params)
+            translate = _get_vector(attrs["tr"], **params)
+            translation = matrix @ (translate + translation)
+            rotation = matrix @ rotation
+        return rotation, translation
+
+
+def obs_angles(r):
+    azimuth = atan2(r[2], r[1])
+    altitude = atan2(r[0], sqrt(r[2] ** 2 + r[1] ** 2))
+    return altitude, azimuth
---
+++ orbits/orbit.py
@@ -0,0 +1,135 @@
+from fractions import Fraction
+from math import pi, cos, sin, atan, sqrt, tan
+
+import numpy as np
+from cproperty import cproperty
+from pyunitx.angle import degrees
+from pyunitx.constants import G
+from pyunitx.length import meters
+from pyunitx.mass import kilograms
+from pyunitx.time import days, seconds
+
+
+class EllipticalOrbit:
+    def __init__(self, inclination: degrees, eccentricity, M: kilograms, raan: degrees, arg_periapsis: degrees,
+                 period: days):
+        self.inclination = inclination.to_radians()
+        self.eccentricity = eccentricity
+        self.orbited_mass = M
+        self.raan = raan.to_radians()
+        self.arg_periapsis = arg_periapsis.to_radians()
+        self.period = period.to_seconds()
+
+    @cproperty
+    def periapsis(self):
+        return self.semimajor * (1 - self.eccentricity)
+
+    @cproperty
+    def apoapsis(self):
+        return self.semimajor * (1 + self.eccentricity)
+
+    @cproperty
+    def semimajor(self):
+        norm = self.period / (2 * pi)
+        return (self.mu * norm ** 2) ** Fraction(1, 3)
+
+    @cproperty
+    def semiminor(self):
+        return self.semimajor * sqrt(1 - self.eccentricity ** 2)
+
+    @cproperty
+    def mu(self):
+        return G * self.orbited_mass
+
+    @cproperty
+    def angular_momentum_mag(self):
+        return (self.mu * self.semimajor * (1 - self.eccentricity ** 2)) ** Fraction(1, 2)
+
+    @cproperty
+    def p(self):
+        return self.periapsis * (1 + self.eccentricity)
+
+    def evaluate(self, angle):
+        # Returns (_r,_v) in body-pointed frame [r,theta]
+        # Which is a rotation by angle around 3 from the central object's frame
+        if isinstance(angle, degrees):
+            thetastar = angle.to_radians()
+        else:
+            thetastar = angle
+        distance = self.p / (1 + self.eccentricity * cos(thetastar))
+        r = np.array([distance, meters(0), meters(0)])
+        vr = (self.mu / self.angular_momentum_mag) * self.eccentricity * sin(thetastar)
+        vtheta = (self.mu / self.angular_momentum_mag) * (1 + self.eccentricity * cos(thetastar))
+        v = np.array([vr, vtheta, meters(0) / seconds(1)])
+        return r.reshape((3, 1)), v.reshape((3, 1))
+
+    def eval_unitless(self, angle):
+        r, v = self.evaluate(angle)
+        r = np.vectorize(lambda k: float(k.to_meters().value))(r)
+        v = np.vectorize(lambda k: float(k.to_meters_per_second().value))(v)
+        return r, v
+
+    def find_angle(self, time_since_periapsis: days):
+        # See Orbital Mechanics for Engineers eq. 3.17
+        n = 2 * pi / self.period.to_seconds()
+        Me = float(n * time_since_periapsis.to_seconds())
+        ecc_anomaly = newtons_method(lambda E: E - self.eccentricity * sin(E) - Me,
+                                     lambda E: 1 - self.eccentricity * cos(E),
+                                     pi / 2)
+        ecc = sqrt((1 + self.eccentricity) / (1 - self.eccentricity))
+        arg = ecc * tan(ecc_anomaly / 2)
+        return 2 * atan(arg)
+
+
+class Body:
+    def __init__(self, tilt, mass=None, radius=None, density=None, circumference=None):
+        self.tilt = tilt.to_radians()
+        if radius:
+            self.radius = radius
+            self.circumference = 2 * pi * radius
+        elif circumference:
+            self.circumference = circumference
+            self.radius = circumference / (2 * pi)
+        else:
+            raise ValueError("Must have exactly one of radius/circumference")
+        if mass:
+            self.mass = mass
+            self.density = mass / self.volume
+        elif density:
+            self.density = density
+            self.mass = self.volume * density
+        else:
+            raise ValueError("Must have exactly one of mass/density")
+
+    @property
+    def volume(self):
+        return (4 / 3) * pi * self.radius ** 3
+
+
+def newtons_method(f, fprime, x0):
+    eps = 1e-6
+    x = x0
+    for i in range(100):
+        delta = f(x) / fprime(x)
+        x = x - delta
+        if abs(delta) < eps:
+            return x
+    raise ValueError("Could not converge in 100 iterations, probably a problem")
+
+
+if __name__ == '__main__':
+    moon1 = EllipticalOrbit(
+        inclination=degrees(72),
+        period=days(13.37),
+        raan=degrees(58),
+        arg_periapsis=degrees(121),
+        eccentricity=0.133,
+        M=kilograms("3.33469e24")
+    )
+    print(moon1.semimajor, moon1.semiminor, moon1.periapsis, moon1.apoapsis)
+    th = moon1.find_angle(days(0))
+    print(th, moon1.evaluate(th))
+    th = moon1.find_angle(days(4))
+    print(th, moon1.evaluate(th))
+    th = moon1.find_angle(days(13.37 / 2))
+    print(th, moon1.evaluate(th))
