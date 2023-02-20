d2bf4026a314ca7401c2f8fc1b00681506adf968
Nick Thurmes <nthurmes@gmail.com>
2023-02-19 18:07:27 -0700

    Document the central functions so I can remember how they work later

--- orbits/frames.py
+++ orbits/frames.py
@@ -5,6 +5,15 @@ import numpy as np
 
 
 def rotations(pairs):
+    """Calculate the rotation matrix from a sequence of (axis, angle) pairs.
+
+    :param pairs: An ordered sequence of (axis, angle) pairs where axis is in
+        {1, 2, 3} and angle is given in radians. The rotations are applied left
+        to right, so [(3, pi), (1, -pi/2)] represents a counterclockwise
+        rotation around Z by 180 degrees then a clockwise rotation around 1 by
+        90 degrees. This example puts x'=-x, y'=z, z'=y.
+    :return: A 3x3 numpy matrix for the full rotation.
+    """
     dcms = {
         1: lambda a: np.array([[1, 0, 0], [0, cos(a), sin(a)], [0, -sin(a), cos(a)]]),
         2: lambda a: np.array([[cos(a), 0, -sin(a)], [0, 1, 0], [sin(a), 0, cos(a)]]),
@@ -18,6 +27,19 @@ def rotations(pairs):
 
 
 def _get_dcm(transform, **params) -> np.ndarray:
+    """Calculate rotation matrix either directly or lazily.
+
+    :param transform: One of three shapes:
+        1. A 3x3 rotation matrix already calculated. Sometimes it's just easier
+           to manually calculate a simple flip.
+        2. A list of (axis, angle) pairs. This is fed to :py:`rotations`.
+        3. A function that takes only keyword arguments. This is called with
+           the keyword arguments passed into this function. The return value
+           must be one of the two options above.
+    :keyword params: A set of keyword arguments if the transform must be lazily
+        evaluated.
+    :return: A 3x3 numpy matrix for the rotation.
+    """
     if callable(transform):
         # a function that returns either a matrix or axis, angle pairs
         return _get_dcm(transform(**params), **params)
@@ -29,6 +51,19 @@ def _get_dcm(transform, **params) -> np.ndarray:
 
 
 def _get_vector(transform, **params) -> np.ndarray:
+    """Calculate offset vector either directly or lazily.
+
+    :param transform: One of three shapes:
+        1. A numpy 3x1 (column) vector.
+        2. A 3-element numeric sequence, which will be turned into a column
+            vector.
+        3. A function that takes only keyword arguments. This is called with
+           the keyword arguments passed into this function. The return value
+           must be one of the two options above.
+    :keyword params: A set of keyword arguments if the transform must be lazily
+        evaluated.
+    :return: A 3x1 numpy vector representing an offset.
+    """
     if callable(transform):
         return _get_vector(transform(**params), **params)
     if isinstance(transform, np.ndarray) and transform.shape == (3, 1):
@@ -42,8 +77,26 @@ class FrameSystem:
         self.graph = nx.DiGraph()
 
     def add_frame(self, name, parent=None, rotation=None, translation=None, inv_translation=None):
-        # translation is expressed in the parent frame and is a vector from parent to child
-        # inv_translation is also vector from parent to child, but in the child frame
+        """Define a reference frame within the system.
+
+        For rotation and translation/inv_translation, you must use the function
+        style for any time-variant values. These functions must take any number
+        of keyword arguments, but only care about particular named ones that
+        are relevant to the calculation. The set of names that are used is very
+        important for evaluating a transform later.
+
+        :param name: A unique name for this frame.
+        :param parent: The name of the existing frame that this new one is
+            defined against.
+        :param rotation: The rotation matrix from the parent frame to the new
+            frame. Can take anything that :py:`_get_dcm` can.
+        :param translation: The vector from the origin of the parent frame to
+            the origin of the daughter frame, given in the parent frame.
+            Mutually exclusive with inv_translation.
+        :param inv_translation: The vector from the origin of the parent frame
+            to the origin of the daughter frame, given in the daughter frame.
+            Mutually exclusive with translation.
+        """
         self.graph.add_node(name)
         if not parent:
             return
@@ -54,7 +107,6 @@ class FrameSystem:
         if translation is None and inv_translation is None:
             translation = _get_vector([0, 0, 0])
         if translation is not None:
-            # translation = lambda **p: _get_vector(translation, **p)
             inverse_translation = lambda **p: _get_dcm(rotation, **p) @ -_get_vector(translation, **p)
         elif inv_translation is not None:
             inverse_translation = lambda **p: -_get_vector(inv_translation, **p)
@@ -65,10 +117,27 @@ class FrameSystem:
         inverse_rotation = lambda **p: _get_dcm(rotation, **p).transpose()
         self.graph.add_edge(name, parent, tr=inverse_translation, rot=inverse_rotation)
 
-    def transform(self, a, b, default_units=float, **params):
+    def transform(self, a, b, **params):
+        """Calculate the total rotation and translation from one frame to another.
+
+        You must pass all keyword arguments that are required by any frame
+        between the start and end.
+
+        :param a: The name of the frame to transform from.
+        :param b: The name of the frame to transform to.
+        :keyword params: Any number of keyword arguments, that will be passed to
+            any transforms that require dynamic evaluation.
+        :return: A tuple of:
+            1. The total rotation matrix between the two frames. That is,
+               if you have a vector in the start frame *v* and multiply it by
+               this matrix *R* like *R @ v*, you will get the equivalent vector
+               seen from the end frame.
+            2. The translation between the origin of the start frame and the
+               origin of the end frame, expressed in the end frame.
+        """
         path = nx.shortest_path(self.graph, a, b)
         rotation = np.identity(3)
-        translation = np.array([default_units(0), default_units(0), default_units(0)]).reshape((3, 1))
+        translation = np.array([0, 0, 0]).reshape((3, 1))
         for edge in zip(path[:-1], path[1:]):
             attrs = self.graph.get_edge_data(*edge)
             matrix = _get_dcm(attrs["rot"], **params)
@@ -79,8 +148,20 @@ class FrameSystem:
 
 
 def altitude(r):
+    """Calculate the altitude angle defined by the vector's direction.
+
+    :param r: The column vector of position.
+    :return: The altitude, in radians. This is defined as the angle between the
+        vector and the y-z plane.
+    """
     return atan2(r[0], sqrt(r[2] ** 2 + r[1] ** 2))
 
 
 def azimuth(r):
+    """Calculate the azimuth angle defined by the vector's direction.
+
+    :param r: The column vector of position.
+    :return: The azimuth, in radians. This is defined as the angle from y in the
+        y-z plane.
+    """
     return atan2(r[2], r[1])
--- orbits/orbit.py
+++ orbits/orbit.py
@@ -7,18 +7,37 @@ from pyunitx.angle import degrees
 from pyunitx.constants import G
 from pyunitx.length import meters
 from pyunitx.mass import kilograms
-from pyunitx.time import days
+from pyunitx.time import days, seconds
 
 from frames import rotations
 from solvers import newtons_method
 
 
 class EllipticalOrbit:
-    def __init__(self, inclination: degrees, eccentricity, M: kilograms, raan: degrees, arg_periapsis: degrees,
+    def __init__(self, inclination: degrees, eccentricity, mass: kilograms, raan: degrees, arg_periapsis: degrees,
                  period: days, epoch_position: degrees):
+        """Define an elliptical orbit.
+
+        :param inclination: The angle between the north pole of the central body
+            and the normal to the orbital plane. In [0, 90] degrees.
+        :param eccentricity: Ellipse eccentricity. Dimensionless, in [0, 1).
+            0 means circular orbit and 1 is escaped parabolic.
+        :param mass: The mass of the central body.
+        :param raan: The right ascension of the ascending node. Undefined when
+            inclination is zero, since there are no nodes. Physically, this is
+            the central-body longitude at which the orbiter crosses the
+            equatorial plane going north. Unbounded, though obviously it's mod
+            360 degrees.
+        :param arg_periapsis: The argument of periapsis. Undefined when raan is.
+            Physically, this is the angle between the ascending node and
+            periapsis. Unbounded, though obviously it's mod 360 degrees.
+        :param period: The orbital period of the body.
+        :param epoch_position: The true anomaly of the body along its orbit at
+            the time of the epoch.
+        """
         self.inclination = inclination.to_radians()
         self.eccentricity = eccentricity
-        self.orbited_mass = M.to_kilograms()
+        self.orbited_mass = mass.to_kilograms()
         self.raan = raan.to_radians()
         self.arg_periapsis = arg_periapsis.to_radians()
         self.period = period.to_seconds()
@@ -54,7 +73,13 @@ class EllipticalOrbit:
         return self.periapsis * (1 + self.eccentricity)
 
     def evaluate(self, angle):
-        # Returns (_r,_v) in central object frame
+        """Calculate the position and velocity at any point in the orbit.
+
+        :param angle: The true anomaly along the orbit.
+        :return: A tuple with two vectors, both expressed in the orbit's perifocal frame:
+            1. The position, in meters.
+            2. The velocity, in meters per second.
+        """
         if isinstance(angle, degrees):
             thetastar = angle.to_radians()
         else:
@@ -63,14 +88,23 @@ class EllipticalOrbit:
         mu = float(self.mu.to_meters_cubed_per_second_squared().value)
         h = float(self.angular_momentum_mag.to_meters_squared_per_second().value)
         distance = p / (1 + self.eccentricity * cos(thetastar))
+        # r, theta make a natural body-centric frame
         r = np.array([distance, 0, 0])
         vr = (mu / h) * self.eccentricity * sin(thetastar)
         vtheta = (mu / h) * (1 + self.eccentricity * cos(thetastar))
         v = np.array([vr, vtheta, 0])
+        # To transform from body to perifocal frame, make the rotation matrix
+        # from perifocal to body then invert it
         Rbc = rotations([(3, thetastar)]).transpose()
         return Rbc @ r.reshape((3, 1)), Rbc @ v.reshape((3, 1))
 
-    def find_angle(self, time_since_periapsis: days):
+    def find_angle(self, time_since_periapsis: days) -> float:
+        """Find the true anomaly at any time along the orbit.
+
+        :param time_since_periapsis: How long has elapsed since periapsis. Can
+            be negative so it's time before periapsis.
+        :return: The true anomaly, in radians.
+        """
         # See Orbital Mechanics for Engineers eq. 3.17
         n = 2 * pi / self.period.to_seconds()
         if self.eccentricity == 0:
@@ -85,7 +119,12 @@ class EllipticalOrbit:
         arg = ecc * tan(ecc_anomaly / 2)
         return 2 * atan(arg)
 
-    def find_time(self, theta: float):
+    def find_time(self, theta: float) -> seconds:
+        """Find the time that has elapsed since periapsis given a true anomaly.
+
+        :param theta: The true anomaly, in radians.
+        :return: The time since periapsis, in pyunitx seconds.
+        """
         n = 2 * pi / self.period.to_seconds()
         if self.eccentricity == 0:
             return theta / n
@@ -98,6 +137,25 @@ class EllipticalOrbit:
 class Body:
     def __init__(self, precession: degrees, tilt: degrees, epoch_spin: degrees, spin_rate, mass=None, radius=None,
                  density=None, circumference=None):
+        """Define the important parameters of a body in an orbit.
+
+        Among mass, radius, density, and circumference, any two can be defined.
+
+        :param precession: At the epoch, the angle between the tilt direction
+            and its natural frame.
+        :param tilt: The angle between the body's north and the north of the
+            body it's orbiting. The direction of the tilt is defined by
+            precession.
+        :param epoch_spin: At the epoch, how much the body has spun about its
+            own axis.
+        :param spin_rate: How fast the body is spinning, in radians per second.
+        :param mass: The mass of the body, in pyunitx kilograms.
+        :param radius: The radius of the body (assumed to be a perfect sphere),
+            in pyunitx meters.
+        :param density: The density of the body, in pyunitx kilograms per cubic
+            meter.
+        :param circumference: The circumference of the body, in pyunitx meters.
+        """
         self.precession = precession.to_radians()
         self.tilt = tilt.to_radians()
         self.epoch_spin = epoch_spin.to_radians()
@@ -110,7 +168,7 @@ class Body:
     @cached_property
     def radius(self):
         if self._radius:
-            return self._radius
+            return self._radius.to_meters()
         if self.circumference:
             return self.circumference / (2 * pi)
         if self.mass and self.density:
@@ -120,21 +178,21 @@ class Body:
     @cached_property
     def circumference(self):
         if self._circumference:
-            return self._circumference
+            return self._circumference.to_meters()
         if self.radius:
             return 2 * pi * self.radius
 
     @cached_property
     def mass(self):
         if self._mass:
-            return self._mass
+            return self._mass.to_kilograms()
         if self.density:
             return self.volume * self.density
 
     @cached_property
     def density(self):
         if self._density:
-            return self._density
+            return self._density.to_kilograms_per_meter_cubed()
         if self.mass:
             return self.mass / self.volume
 
@@ -144,12 +202,25 @@ class Body:
 
     @cached_property
     def gravity(self):
+        """The gravitational acceleration at the surface of the body."""
         return G * self.mass / self.radius ** 2
 
-    def roche_limit(self, satellite: 'Body'):
+    def roche_limit(self, satellite: 'Body') -> meters:
+        """The minimum orbital radius at which a moon breaks up into rings.
+
+        :param satellite: The orbiter.
+        :return: The Roche limit, in pyunitx meters.
+        """
         return self.radius * float(2 * self.density / satellite.density) ** Fraction(1, 3)
 
-    def surface(self, lat: degrees, lon: degrees):
+    def surface(self, lat: degrees, lon: degrees) -> np.ndarray:
+        """The position of an object on the surface of the body.
+
+        :param lat: The latitude of the object.
+        :param lon: The longitude of the object.
+        :return: A 3x1 numpy vector of floats representing meters in the body's
+            MEN frame.
+        """
         lat = lat.to_radians()
         lon = lon.to_radians()
         direction = np.array([cos(lat) * cos(lon), cos(lat) * sin(lon), sin(lat)]).reshape((3, 1))
